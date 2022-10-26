14e46b733d5e88868688c5778886e8cc51bb635b
James Best <jim@justjibba.net>
2022-10-26 05:43:36 +0100

    Add get transfers

--- db/query/transfers.sql
+++ db/query/transfers.sql
@@ -5,3 +5,13 @@ INSERT INTO transfers (
   $1, $2, $3
 )
 RETURNING *;
+
+-- name: GetTransfer :one
+SELECT * FROM transfers
+WHERE id = $1 LIMIT 1;
+
+-- name: ListTransfers :many
+SELECT * FROM transfers
+ORDER BY id
+LIMIT $1 OFFSET $2;
+
